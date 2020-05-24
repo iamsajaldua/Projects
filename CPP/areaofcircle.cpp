eb532fd563f1bd20997acdb5bfee52169fde173b
Sajal Dua <iamsajaldua@gmail.com>
2020-05-24 16:11:33 +0530

    master

---
+++ CPP/areaofcircle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdlib>
+int main()
+{
+    system("cls");
+    double radius,area,perimeter;
+    cout<<"radius of asshole(circle):";
+    cin>>radius
+    perimeter = 2*3.1415926535897932384626433832795*radius;
+    area = 3.1415926535897932384626433832795*radius*radius;
+    cout<<"Area of Asshole(circle):";
+    cout<<Area;
+    cout<<"\nPerimeter of Asshole(Circle):";
+    cout<<perimeter;
+    return 0;
+    }
