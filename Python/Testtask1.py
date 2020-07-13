26122988c2b321c943d6c7b87445fa7437b2abd8
Sajal Dua <iamsajaldua@gmail.com>
2020-07-13 22:58:47 +0530

    master

--- Python/Testtask1.py
+++ Python/Testtask1.py
@@ -2,4 +2,4 @@ w = 5 + 9
 x = 7 + 11
 y = 67 + 76 
 z = 5787 + 7566
-print("Sajal Dua is a God")
+print("Sajal Dua aka the (GOD) salal dua is the best so fuck the whole human verse Sajal Dua aka Big dic* SD holla")
